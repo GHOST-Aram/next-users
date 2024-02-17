50387973a9518e189b989d5c05f6ef24075d1ff4
GHOST-Aram <owinoojala@gmail.com>
2024-02-17 12:42:38 +0300

    feat: Create User model
          - Describe User Interface and extend it to create schema
          - Create a password hashing pre-save function for user Schema
          - Create a password verification schema method for user

---
+++ src/app/api/data-access/model.ts
@@ -0,0 +1,37 @@
+import { HydratedDocument, Model, Schema, model } from "mongoose"
+import { compare, hash } from 'bcrypt'
+
+
+interface User{
+    name: string
+    email: string
+    password: string
+}
+
+interface UserMethods{
+    hasValidPassword: (password: string) => Promise<boolean>
+}
+export type UserModel = Model<User>
+
+const userSchema = new Schema<User, UserModel>({
+    name: String,
+    email: String,
+    password:String
+})
+
+userSchema.pre('save', async function(next){
+    const hashedPassword = await hash(this.password, 10)
+    this.password = hashedPassword
+
+    next()
+})
+
+userSchema.method('hasValidPassword', async function(password: string): Promise<boolean>{
+    const isValidPassword = await compare(password, this.password)
+
+    return isValidPassword
+})
+
+export type HydratedUserModel = HydratedDocument<User>
+
+export const User = model<User,UserModel>('User', userSchema)
